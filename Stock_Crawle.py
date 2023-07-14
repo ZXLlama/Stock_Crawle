4476e6702d8b0929cca3a33d85aa38a2d4d0ce0a
ZXSNOW <ytllama1225@gmail.com>
2023-07-14 13:23:24 +0800

    V1.1

---
+++ Stock_Crawle.py
@@ -0,0 +1,72 @@
+import xml.etree.ElementTree as ET
+import requests
+import time
+from openpyxl import Workbook
+def fs(sheet,data,row): #填寫工作表中的內容(fill sheet)
+    for column, value in enumerate(data,1): #重複讀取資料
+        sheet.cell(row = row,column = column,value = value) #將資料放置在行(row)和列(column)上，其格子填寫value值
+def rd(startYear,startMonth,endYear,endMonth,day = "01"): #returnStrDateList 組合日期字串
+    result = []
+    if startYear == endYear:
+        for month in range(startMonth,endMonth+1):
+            month = str(month)
+            if len(month) == 1:
+                month = "0" + month
+            result.append(str(startYear),month,day)
+        return result
+    for year in range(startYear,endYear+1):
+        if year == startYear: #當年份=起始年份時的月份
+            for month in range(startMonth,13):
+                month = str(month)
+                if len(month) == 1:
+                    month = "0" + month
+                result.append(str(year)+month+day)
+
+        elif year == endYear: #當年份=結束年份時的月份
+            for month in range(1,endMonth+1):
+                month = str(month)
+                if len(month) == 1:
+                    month = "0" + month
+                result.append(str(year)+month+day)
+
+        else: #當年份=除起始年份及結束年分時的月份
+            for month in range(1,13):
+                month = str(month)
+                if len(month) == 1:
+                    month = "0" + month
+                result.append(str(year)+month+day)
+
+    return result
+tree = ET.parse('data.xml') #解析XML檔案1
+root = tree.getroot() #解析XML檔案2
+paramsDict = {} #建立空字典(主要使用之字典)
+for child in root: # 迭代每個元素
+    tag_name = child.tag #獲取tag名稱
+    tag_value = child.text #獲取tag值 
+    paramsDict[tag_name] = tag_value  #儲存至字典
+Fields = ["日期","成交股數","成交金額","開盤價","最高價","最低價","收盤價","漲跌價差","成交筆數"] #每列標題
+wb = Workbook() #建立Excel檔案
+sheet = wb.active
+sheet.title = "Fields" #工作表名稱
+fs(sheet,Fields,1) #在第一行寫入列標題
+startYear,startMonth = int(paramsDict["startYear"]),int(paramsDict["startMonth"]) #讀取字典中的起始日期並轉換為正整數
+endtYear,endMonth = int(paramsDict["endYear"]),int(paramsDict["endMonth"]) #讀取字典中的結束日期並轉換為正整數
+yl = rd(startYear,startMonth,endtYear,endMonth) #年份清單(YearList)
+row = 2 #從第二行開始
+url = 'https://www.twse.com.tw/rwd/zh/afterTrading/STOCK_DAY' #台灣證交所網址前綴
+for YearMonth in yl: #重複抓取資料
+    rq = rq = requests.get(url,params={
+    "response":"json",
+    "date":YearMonth,
+    "stockNo":paramsDict["stockNo"], #股票代碼(亞泥1102)
+})
+    jsonData = rq.json() #把資料轉換成json檔
+    dailyPriceList = jsonData.get("data",[])
+    for dailyPrice in dailyPriceList:
+        fs(sheet,dailyPrice,row)
+        row += 1 #換行
+        print(row)
+    delay = paramsDict["delaySec"]
+    time.sleep(eval(delay)) #等一秒避免被拒絕訪問
+name = paramsDict["excelName"]
+wb.save(name + ".xlsx")
--- ui.py
+++ ui.py
@@ -4,46 +4,45 @@ from tkinter import Scale
 import xml.etree.ElementTree as ET
 import tkinter.messagebox as messagebox
 import datetime
+import html
 
 def update_xml():
-    # 获取用户界面中的数值
+    # 獲取用戶界面中的數值
     start_year = start_year_combobox.get()
     start_month = start_month_combobox.get()
     end_year = end_year_combobox.get()
     end_month = end_month_combobox.get()
     stock_no = stock_no_entry.get()
-    excel_name = excel_name_entry.get()
+    excel_name = html.unescape(excel_name_entry.get())
     delay_sec = delay_scale.get()
 
-    # 判断起始年月是否小于结束年月，若是则交换
+    # 判斷起始年月是否小於結束年月，若是則交換
     if int(start_year) > int(end_year) or (int(start_year) == int(end_year) and int(start_month) > int(end_month)):
         start_year, end_year = end_year, start_year
         start_month, end_month = end_month, start_month
-        messagebox.showwarning("错误", "起始年月份不能大于结束年月份！已自动调整。")
+        messagebox.showwarning("錯誤", "起始年月份不能大於結束年月份！已自動調整。")
 
-    # 更新XML文件中的数值
-    with open('data.xml', 'r', encoding='UTF-8') as file:
-        tree = ET.parse(file)
-        root = tree.getroot()
-        root.find('startYear').text = start_year
-        root.find('startMonth').text = start_month
-        root.find('endYear').text = end_year
-        root.find('endMonth').text = end_month
-        root.find('stockNo').text = stock_no
-        root.find('excelName').text = excel_name
-        root.find('delaySec').text = str(delay_sec)
-    with open('data.xml', 'w', encoding='UTF-8') as file:
-        tree.write(file)
+    # 更新XML文件中的數值
+    tree = ET.parse('data.xml')
+    root = tree.getroot()
+    root.find('startYear').text = start_year
+    root.find('startMonth').text = start_month
+    root.find('endYear').text = end_year
+    root.find('endMonth').text = end_month
+    root.find('stockNo').text = stock_no
+    root.find('excelName').text = excel_name
+    root.find('delaySec').text = str(delay_sec)
+    tree.write('data.xml', encoding='UTF-8')  # 指定編碼為UTF-8
     messagebox.showinfo("更新成功", "XML文件已成功更新！")
 
-# 创建主窗口
+# 創建主窗口
 window = tk.Tk()
 window.title("每日股票竊取器")
 
-# 设置窗口宽度
+# 設置窗口寬度
 window.geometry("400x400")
 
-# 创建输入字段和标签
+# 創建輸入字段和標簽
 start_year_label = tk.Label(window, text="起始年份:")
 start_year_label.pack()
 
@@ -77,16 +76,16 @@ excel_name_label.pack()
 excel_name_entry = tk.Entry(window)
 excel_name_entry.pack()
 
-# 创建滑块用于调整delaySec参数
-delay_label = tk.Label(window, text="延迟秒数:")
+# 創建滑塊用於調整delaySec參數
+delay_label = tk.Label(window, text="延遲秒數:")
 delay_label.pack()
-delay_scale = Scale(window, from_=0.1, to=5, resolution=0.1, orient=tk.HORIZONTAL)
-delay_scale.set(1.0)  # 默认值为1.0
+delay_scale = Scale(window, from_=0.1, to=5, resolution=0.01, orient=tk.HORIZONTAL, length=300)
+delay_scale.set(1.0)  # 默認值為1.0
 delay_scale.pack()
 
-# 创建更新按钮
+# 創建更新按鈕
 update_button = tk.Button(window, text="更新", command=update_xml, width=10)
 update_button.pack(pady=10)
 
-# 启动主循环
+# 啟動主循環
 window.mainloop()
